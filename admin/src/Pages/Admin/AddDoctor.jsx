a1bbbd7ca9c1a72f5059bb6476246da65771be1a
mdmaj <flyingheartkqr07@gmail.com>
2026-09-12 23:07:37 +0530

    fix: correct formatting and spacing in AddDoctor component and AdminContextProvider

--- admin/src/Pages/Admin/AddDoctor.jsx
+++ admin/src/Pages/Admin/AddDoctor.jsx
@@ -4,14 +4,17 @@ import { useState } from "react";
 import { assets } from "../../assets/assets_admin/assets";
 
 const AddDoctor = () => {
-  const [docImg, setDocImg]= useState(false) 
-  const [name, setName] = useState('')
-  const [email,setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [experience, setExperience] = useState('')
-  const [fees, setFess] = useState('')
-  const [about,setAbout] = useState('')
-  const [degree, setDegree]= useState('')
+  const [docImg, setDocImg] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [experience, setExperience] = useState("1 Year");
+  const [fees, setFess] = useState("");
+  const [about, setAbout] = useState("");
+  const [degree, setDegree] = useState("");
+  const [speciality, setSpeciality] = useState("General Physician");
+  const [address1, setAddress1] = useState("");
+  const [address2, setAddress2] = useState("");
   return (
     <form className="m-5 w-full">
       <p className="mb-3 text-lg font-medium">Add Doctor</p>
@@ -20,11 +23,11 @@ const AddDoctor = () => {
           <label htmlFor="doc-img">
             <img
               className="w-16 bg-gray-100 rounded-full cursor-pointer"
-              src={assets.upload_area}
+              src={docImg ?URL.createObjectURL(docImg) :assets.upload_area}
               alt=""
             />
           </label>
-          <input type="file" id="doc-img" hidden />
+          <input onChange={(e)=>(e.target.files[0])} type="file" id="doc-img" hidden />
           <p>
             Upload doctor <br />
             picture
@@ -44,11 +47,21 @@ const AddDoctor = () => {
             </div>
             <div className="flex-1 flex flex-col gap-1">
               <p>Doctor Email</p>
-              <input className="border rounded px-3 py-2" type="email" placeholder="Email" required />
+              <input
+                className="border rounded px-3 py-2"
+                type="email"
+                placeholder="Email"
+                required
+              />
             </div>
             <div className="flex-1 flex flex-col gap-1 ">
               <p>Doctor Password</p>
-              <input className="border rounded px-3 py-2" type="password" placeholder="Password" required />
+              <input
+                className="border rounded px-3 py-2"
+                type="password"
+                placeholder="Password"
+                required
+              />
             </div>
             <div className="flex-1 flex flex-col gap-1">
               <p>Experience</p>
@@ -68,7 +81,12 @@ const AddDoctor = () => {
 
             <div className="flex-1 flex flex-col gap-1">
               <p>Fees</p>
-              <input className="border rounded px-3 py-2" type="number" placeholder="Fees" required />
+              <input
+                className="border rounded px-3 py-2"
+                type="number"
+                placeholder="Fees"
+                required
+              />
             </div>
           </div>
 
@@ -87,21 +105,43 @@ const AddDoctor = () => {
 
             <div className="flex-1 flex flex-col gap-1">
               <p>Education</p>
-              <input className="border rounded px-3 py-2" type="text" placeholder="Education" required />
+              <input
+                className="border rounded px-3 py-2"
+                type="text"
+                placeholder="Education"
+                required
+              />
             </div>
 
             <div className="flex-1 flex flex-col gap-1">
               <p>Address</p>
-              <input className="border rounded px-3 py-2" type="text" placeholder="address 1" required />
-              <input className="border rounded px-3 py-2" type="text" placeholder="address 2" required />
+              <input
+                className="border rounded px-3 py-2"
+                type="text"
+                placeholder="address 1"
+                required
+              />
+              <input
+                className="border rounded px-3 py-2"
+                type="text"
+                placeholder="address 2"
+                required
+              />
             </div>
           </div>
         </div>
         <div>
           <p className="mt-4 mb-2">About Doctor</p>
-          <textarea className="border rounded px-4 pt-2 w-full" placeholder="Write about doctor" rows={5} id=""></textarea>
+          <textarea
+            className="border rounded px-4 pt-2 w-full"
+            placeholder="Write about doctor"
+            rows={5}
+            id=""
+          ></textarea>
         </div>
-        <button className="bg-primary px-10 py-3 mt-4 text-white rounded-full">Add Doctor</button>
+        <button className="bg-primary px-10 py-3 mt-4 text-white rounded-full">
+          Add Doctor
+        </button>
       </div>
     </form>
   );
--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -4,7 +4,7 @@ import { AdminConext } from "./AdminContext.js";
 const AdminConextProvider = (props) => {
   const [aToken, setAToken] = useState(
     localStorage.getItem("aToken") ? localStorage.getItem("aToken") : "",
-  );
+   );
 
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
